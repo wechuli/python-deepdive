098287b5954c23ac2c5ace77b2f2eef16534f341
Wechuli <wechulipaul@yahoo.com>
2019-12-14 23:11:43 +0300

    more on partial functions with complicated parameters

--- Part 1/Section 06 - First-Class Functions/code/partial_functions.py
+++ Part 1/Section 06 - First-Class Functions/code/partial_functions.py
@@ -11,3 +11,18 @@ def f(x, y):
 
 my_func(10, 20, 30)
 f(20, 30)
+
+f_partial = partial(my_func, 10)
+
+f_partial(56, 45)
+
+
+def my_func(a, b, *args, k1, k2, **kwargs):
+    print(a, b, args, k1, k2, kwargs)
+
+
+my_func(10, 20, 100, 200, 300, k1='a', k2='b', another=[123, 434], anoth="ff")
+
+my_func_partial = partial(my_func, 10, k1='k1-changed', verse="poem")
+
+my_func_partial(20, 100, 45, k2='k2-previous')
