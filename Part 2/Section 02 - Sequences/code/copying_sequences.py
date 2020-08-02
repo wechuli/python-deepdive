51f5fcb3441612d5c4fc656052535b4aad8712d0
Wechuli <wechulipaul@yahoo.com>
2020-08-02 21:04:29 +0300

    deep copies for custom objects

--- Part 2/Section 02 - Sequences/code/copying_sequences.py
+++ Part 2/Section 02 - Sequences/code/copying_sequences.py
@@ -25,3 +25,31 @@ mynumbers = [89.55, 1, 2, 3]
 deep_numbers = deepcopy(mynumbers)
 
 print(id(mynumbers[0]), id(deep_numbers[0]))
+
+
+# copying custom objects
+
+class Point:
+    def __init__(self, x: float, y: float) -> void:
+        self.x = x
+        self.y = y
+
+    def __repr__(self) -> str:
+        return f'Point({self.x},{self.y})'
+
+
+class Line:
+    def __init__(self, p1: Point, p2: Point) -> void:
+        self.p1 = p1
+        self.p2 = p2
+
+    def __repr__(self) -> str:
+        return f'Line({self.p1.__repr__()},{self.p2.__repr__()})'
+
+
+p1 = Point(0, 0)
+p2 = Point(10, 10)
+line1 = Line(p1, p2)
+line2 = deepcopy(line1)
+
+
